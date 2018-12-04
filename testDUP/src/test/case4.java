cd2486c1189cfecfe9ffb6d81e99ab03aa1b7681
Michael Peters <m.peters@m2mobi.com>
2018-12-04 19:34:57 +0100

    Added some more duplicate tests

--- testDUP/src/test/case4.java
+++ testDUP/src/test/case4.java
@@ -17,4 +17,35 @@ public class case4
     	// comment
     	a4 = 1;
 	}
+    
+    public void a3() {
+    	int a4 = 1;
+	}
+    
+    public void a2() {
+    	int a4 = 1;
+    	a4 = 1;
+	}
+    
+    public void a1() {
+    	int a4 = 1;
+    	a4 = 1;
+    	a4 = 1;
+    	a4 = 1;
+	}
+    
+    public void a5() {
+    	int a4 = 1;
+    	a4 = 1;
+    	a4 = 1;
+    	a4 = 1;
+    	a4 = 1;
+    	a4 = 1;
+    	a4 = 1;
+	}
+}
+
+class Foos4
+{
+
 }
